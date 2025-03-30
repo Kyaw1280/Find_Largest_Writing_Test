dd96851b33a994b09f02648da89366a1bae5747d
han nwe <hannwe@hans-MacBook-Air.local>
2025-03-30 19:24:49 +0100

    Add jest-each test

--- functions.test.js
+++ functions.test.js
@@ -1,3 +1,5 @@
+const each = require('jest-each').default
+
 const {findLargest} = require("./functions")
 
 describe("findLarget", () => { 
@@ -29,6 +31,21 @@ it("indentifys the largest value's index" ,() => {
 
 test(" returns -1 if no value are passed in", () => {
     expect(findLargest([])).toEqual(-1)
+})
+
+each ([
+    [2, [1,2,3]],
+    [0, [-1, -10, -100]],
+    [-1, []],
+    [0, [1000000, 4, 5, 2, 5646, 23234, 234]],
+    [4, [67, -999, 45, 999, 9992323]],
+]).test(`Returns %S when passed %S`, (expected, arr) => {
+    // Arrange
+    const numbers = arr
+    // Act
+    const result = findLargest(numbers)
+    //Assertion
+    expect(result).toBe(expected)
 })
 
 })
